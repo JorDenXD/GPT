90ce7ae32a4e3d85eaac5a3bfa45b56ceff2debf
JorDenXD <98655130+JorDenXD@users.noreply.github.com>
2025-02-12 09:13:52 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -3,9 +3,9 @@ import os
 
 from dotenv import load_dotenv
 
-from plugin_manager import PluginManager
-from openai_helper import OpenAIHelper, default_max_tokens, are_functions_available
-from telegram_bot import ChatGPTTelegramBot
+from .plugin_manager import PluginManager
+from .openai_helper import OpenAIHelper, default_max_tokens, are_functions_available
+from .telegram_bot import ChatGPTTelegramBot
 
 
 def main():
